89ae866bf7155b8276baa56ee075d35ddce8dc96
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2021-11-08 15:02:59 +0530

    food app

---
+++ w45-eval/Food Delivery/component/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return ` <div id="box-1">
+    <a href="index.html">Home</a>
+    <a href="menu.html">Menu</a>
+    <a href="Cart.html">Cart</a>
+    <a href="login.html">Login</a>
+    <a href="signup.html">Signup</a>
+</div>`
+}
+
+export default navbar;
